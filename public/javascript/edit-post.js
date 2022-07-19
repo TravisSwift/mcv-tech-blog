b7cf9c772f5d1415e1791e24792e561369b36c50
Travis Swift <travis.arthur.swift@gmail.com>
2022-07-19 14:54:48 -0500

    refactored code again

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,24 +1,27 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value.trim();
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
+  const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
   ];
-  const response = await fetch(`/api/posts/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      title
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
+
+  const response = await fetch(`/api/posts/${post_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          title,
+          post_content
+      }),
+      headers: {
+          'Content-Type': 'application/json'
+      }
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard/');
+      document.location.replace('/dashboard');
   } else {
-    alert(response.statusText);
+      alert(response.statusText);
   }
 }
 
